aeb84ed972c418bf949f6a1d9eb91d31b8ed9721
Gabriel <ggarcia08@my.dtcc.edu>
2023-12-12 21:14:49 -0500

    implemented banner image and shoe images, + style

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,4 +1,10 @@
 import '../App.css';
+import imageBanner from '../assets/Images/banner.jpg';
+import menShoeImage from '../assets/Images/shoe1.jpg';
+import womenShoeImage from '../assets/Images/shoe2.jpg';
+import kidsShoeImage from '../assets/Images/shoe3.jpg';
+import sportsShoeImage from '../assets/Images/shoe4.jpg';
+
 
 function homePage() {
   return (
@@ -28,6 +34,32 @@ function homePage() {
           </li>
         </ul>
       </div>
+
+      <div class="banner-container">
+        <img src = {imageBanner} class = "banner-item-1" alt="main page banner"></img>
+        <div class = "banner-item-2">
+          <p>Shop the latest trends in fashion</p>
+        </div>
+      </div>
+
+      <div id="collection">
+        <div id="men-option" class="collection-box-1">
+          <img src={menShoeImage} ></img>
+          <h3>Shop Men</h3>
+        </div>
+        <div id="women-option" class="collection-box-2">
+          <img src={womenShoeImage}></img>
+          <h3>Shop Women</h3>
+        </div>
+        <div id="shoe-option" class="collection-box-3">
+          <img src={kidsShoeImage}></img>
+          <h3>Shop Kids</h3>
+        </div>
+        <div id="sports-option" class="collection-box-4">
+          <img src={sportsShoeImage}></img>
+          <h3>Shop Sports</h3>
+        </div>
+        </div>
     </>
   );
 }
