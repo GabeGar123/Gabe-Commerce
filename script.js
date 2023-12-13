6eefd7c5098db7756d9a50ba1587314d18595f3b
Gabriel <ggarcia08@my.dtcc.edu>
2023-12-12 20:45:11 -0500

    added previous project for reference

---
+++ script.js
@@ -0,0 +1,56 @@
+ReactDOM.render (
+    <body>
+        <header class="container">
+            <div class="item-1">
+                <p id = "title">El Gabe</p>
+            </div>
+
+            <ul class="nav-container">
+                <li class="nav-item-1">Home</li>
+                <li class="nav-item-2">Men</li>
+                <li class="nav-item-3">Women</li>
+                <li class="nav-item-4">About</li>
+                <li class="nav-item-5">Cart</li>
+            </ul>
+
+            <div class="item-2">
+                <button id="sign-up-button">Sign up</button>
+                <i class="fa-solid fa-bars"></i>
+            </div>
+        </header>
+
+        <div class="banner-container">
+            <img src = "Images/home-banner.jpg" class = "banner-item-1"></img>
+            <div class = "banner-item-2">
+                <p>Shop the latest trends in fashion</p>
+            </div>
+        </div>
+
+        <div id="collection">
+            <div id="men-option" class="collection-box-1">
+                <img src="Images/shoe1.jpg" ></img>
+                <h3>Shop Men</h3>
+            </div>
+            <div id="women-option" class="collection-box-2">
+                <img src="Images/shoe2.jpg"></img>
+                <h3>Shop Women</h3>
+            </div>
+            <div id="shoe-option" class="collection-box-3">
+                <img src="Images/shoe3.jpg"></img>
+                <h3>Shop Shoes</h3>
+            </div>
+            <div id="cloth-option" class="collection-box-4">
+                <img src="Images/shoe4.jpg"></img>
+                <h3>Shop Clothes</h3>
+            </div>
+        </div>
+
+        <footer>
+            This is the footer
+        </footer>
+    </body>
+    
+
+,document.getElementById("root"))
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-import './App.css'
+
 
 function App() {
-
   return (
     <>
-      <div id="header">
-        <ul id="nav-bar">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#news">Men</a></li>
-          <li><a href="#contact">Women</a></li>
-          <li><a href="#about">Kids</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#about">Sign In</a></li>
-          <li><a href="#about">Cart</a></li>
-        </ul>
-      </div>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
